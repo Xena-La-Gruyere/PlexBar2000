ed122f9f78b16d683076ca1b18d0a5b21b0c3506
Xena-La-Gruyere <ghislain.hudry@gmail.com>
2020-06-07 15:26:07 +0200

    Initialize Dependency injection

--- src/Interface/App.xaml.cs
+++ src/Interface/App.xaml.cs
@@ -1,17 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+using System.ComponentModel;
 using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ReactiveUI;
+using Splat;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
-namespace PlexBar2000
+namespace Interface
 {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+        private MainWindow _window;
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            var hostBuilder = new HostBuilder()
+                .ConfigureServices(ConfigureServices)
+                .ConfigureLogging(ConfigureLogger);
+            _host = hostBuilder.Build();
+            ReactiveInjectionExtensions.Provider = _host.Services;
+
+            _window = _host.Services.GetRequiredService<MainWindow>();
+            _window.Closing += MainWindowOnClosing;
+
+            _window.Show();
+        }
+
+        private void MainWindowOnClosing(object sender, CancelEventArgs e)
+        {
+            ShutDown().GetAwaiter().GetResult();
+        }
+
+        private async Task ShutDown()
+        {
+            await _host.StopAsync();
+
+            Current.Shutdown();
+        }
+
+        private void ConfigureLogger(ILoggingBuilder loggingBuilder)
+        {
+            loggingBuilder.AddDebug();
+
+#if DEBUG
+            loggingBuilder.SetMinimumLevel(LogLevel.Debug);
+#endif
+#if RELEASE
+            loggingBuilder.SetMinimumLevel(Interface.Properties.Settings.Default.Debug ? LogLevel.Debug : LogLevel.Information);
+#endif
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            Locator.CurrentMutable.Register(() => new CustomPropertyResolver(), typeof(ICreatesObservableForProperty));
+            services.AddViewModel<MainWindow, MainViewModel>();
+        }
+    }
+
+
+    static class ReactiveInjectionExtensions
+    {
+        public static IServiceProvider Provider;
+        public static IServiceCollection AddViewModel<TView, TViewModel>(this IServiceCollection serviceCollection)
+            where TViewModel : ReactiveObject
+            where TView : ReactiveWindow<TViewModel>
+        {
+            serviceCollection.AddSingleton<TView>();
+            serviceCollection.AddSingleton<TViewModel>();
+
+            Locator.CurrentMutable.Register(() => Provider.GetRequiredService<TViewModel>(), typeof(TViewModel));
+
+            return serviceCollection;
+        }
+    }
+
+    public class CustomPropertyResolver : ICreatesObservableForProperty
+    {
+        public int GetAffinityForObject(Type type, string propertyName, bool beforeChanged = false)
+        {
+            if (!typeof(FrameworkElement).IsAssignableFrom(type))
+                return 0;
+            var fi = type.GetTypeInfo().GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                .FirstOrDefault(x => x.Name == propertyName);
+
+            return fi != null ? 2 /* POCO affinity+1 */ : 0;
+        }
+
+        public IObservable<IObservedChange<object, object>> GetNotificationForProperty(object sender, System.Linq.Expressions.Expression expression, string propertyName,
+            bool beforeChanged = false, bool suppressWarnings = false)
+        {
+            var foo = (FrameworkElement)sender;
+            return Observable.Return(new ObservedChange<object, object>(sender, expression), new DispatcherScheduler(foo.Dispatcher))
+                .Concat(Observable.Never<IObservedChange<object, object>>());
+        }
     }
 }
--- src/Interface/MainWindow.xaml.cs
+++ src/Interface/MainWindow.xaml.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ReactiveUI;
+using Splat;
 
-namespace PlexBar2000
+namespace Interface
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<MainViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
 
+            ViewModel = Locator.Current.GetService<MainViewModel>();
+
+            this.WhenActivated(dispose =>
+                {
+
+                }
+            );
         }
 
 
