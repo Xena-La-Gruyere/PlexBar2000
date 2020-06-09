6694c52a6f1e0d26b86627eeee83bbb13f0af97b
Ghislain Hudry <ghudry@gestform.com>
2020-06-09 16:27:42 +0200

    Added playlist

---
+++ src/Playlist/IPlaylistService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlexClient.Library.Models;
+
+namespace Playlist
+{
+    public interface IPlaylistService
+    {
+        IObservable<ImmutableArray<TrackModel>> Tracks { get; }
+
+        void AddTracks(params TrackModel[] tracks);
+        void RemoveTracks(params TrackModel[] tracks);
+        void Clear();
+    }
+}
---
+++ src/Playlist/PlaylistService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using PlexClient.Library.Models;
+
+namespace Playlist
+{
+    public class PlaylistService : IPlaylistService
+    {
+        public IObservable<ImmutableArray<TrackModel>> Tracks { get; set; }
+        private readonly BehaviorSubject<ImmutableArray<TrackModel>> _tracks;
+
+        public PlaylistService()
+        {
+            _tracks = new BehaviorSubject<ImmutableArray<TrackModel>>(ImmutableArray<TrackModel>.Empty);
+            Tracks = _tracks.DistinctUntilChanged();
+        }
+
+        public void AddTracks(params TrackModel[] tracks)
+        {
+            _tracks.OnNext(_tracks.Value.AddRange(tracks));
+        }
+
+        public void RemoveTracks(params TrackModel[] tracks)
+        {
+            _tracks.OnNext(_tracks.Value.RemoveRange(tracks));
+        }
+
+        public void Clear()
+        {
+            _tracks.OnNext(_tracks.Value.RemoveRange(ImmutableArray<TrackModel>.Empty));
+        }
+    }
+}
