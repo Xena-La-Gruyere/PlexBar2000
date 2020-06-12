d78a459c12b2f05a5bd2f89034ae98b3e73e7ccb
Xena-La-Gruyere <ghislain.hudry@gmail.com>
2020-06-12 20:10:19 +0200

    Add item to playlist no longer play if player is paused

--- src/ApplicationState/ApplicationStateService.cs
+++ src/ApplicationState/ApplicationStateService.cs
@@ -73,6 +73,7 @@ namespace ApplicationState
             PlayerState = playerStateConn;
 
             var playingTrackConn = _store
+                .Where(s => s.PlayerState.PlayingState == PlayingStateEnum.Playing)
                 .Select(s => s.PlayerState.PlayingTrack)
                 .CombineLatest(Playlist.Select(p => p.SelectMany(a => a.Tracks).ToArray()), 
                     (ind, tracks) => ind < tracks.Length ? tracks[ind] : null)
